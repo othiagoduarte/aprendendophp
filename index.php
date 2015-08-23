bf5c18c96240b6c84bdbe507cbcf3d09d2229f3a
Thiago Duarte <thiago-duarte@outlook.com>
2015-08-23 00:25:33 -0300

    Controle de session

--- index.php
+++ index.php
@@ -1,10 +1,51 @@
 <!DOCTYPE html>
+<?php session_start() ?>
 <html>
 	<head>
 		<title>Aprendendo PHP</title>
 		<meta charset="utf-8">
 	</head>
 	<body>
-		<?php echo "Ola"; ?>
+		
+		<h1>Gerenciador de tarefas</h1>
+		<form>	
+			<fieldset>
+				<legend>Nova Tarefa</legend>
+				<label>Tarefa:
+					<input type="text" name="nome"/>
+					<input type="submit" value="cadastrar"  />
+				</label>
+
+			</fieldset>
+		</form>
+		<?php
+			
+			if (isset($_GET['nome'])) {
+				
+				$_SESSION['lista_tarefa'][] = $_GET['nome'];
+							
+			}
+			
+			$lista_tarefa = array();
+
+			if (isset($_SESSION['lista_tarefa'])) {
+				
+				$lista_tarefa = $_SESSION['lista_tarefa']; 
+				
+			}
+
+
+		?>
+	
+		<table >
+			<tr>
+				<td>Tarefa</td>
+			</tr>
+			<?php foreach ($lista_tarefa as $tarefa) : ?>
+				<tr>
+					<td> <?php echo $tarefa ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
 	</body>
 </html>
