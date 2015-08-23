6bfc0bc8c009aea9b075725683ae6374702708c2
Thiago Duarte <thiago-duarte@outlook.com>
2015-08-23 03:12:29 -0300

    Alteração no formulário de preenchimento

--- index.php
+++ index.php
@@ -1,55 +1,11 @@
-<?php 
-	
-	session_start(); 
-	
-	$lista_tarefa = array();
-	
-
-	if (isset($_GET['nome'])) {
-		
-		$_SESSION['lista_tarefa'][] = $_GET['nome'];
-					
-	}
-
-	if (isset($_SESSION['lista_tarefa'])) {
-		
-		$lista_tarefa = $_SESSION['lista_tarefa']; 
-
-	}
-
-
-?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Aprendendo PHP</title>
 		<meta charset="utf-8">
 	</head>
-	<body>
-		
-		<h1>Gerenciador de tarefas</h1>
-		
-		<form>	
-			<fieldset>
-				<legend>Nova Tarefa</legend>
-				<label>Tarefa:
-					<input type="text" name="nome"/>
-					<input type="submit" value="cadastrar"  />
-				</label>
-
-			</fieldset>
-		</form>
-			
-		<table >
-			<tr>
-				<td>Tarefa</td>
-			</tr>
-			<?php foreach ($lista_tarefa as $tarefa) : ?>
-				<tr>
-					<td> <?php echo $tarefa ?></td>
-				</tr>
-			<?php endforeach; ?>
-		</table>
-	
+	<body>		
+		<h1>404</h1>
+		<p>Esta perdido??</p>
 	</body>
 </html>
